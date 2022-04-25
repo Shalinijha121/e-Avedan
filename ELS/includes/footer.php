f10802ef5b0b041e6a6339d7e7f7581b4d456530
Praduman kumar <75528695+Praduman80@users.noreply.github.com>
2022-04-25 21:49:47 +0530

    all files are uploaded

---
+++ ELS/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | e-Avedan | Developed By <a href="https://kmrpraduman.me">Praduman | Ankit | Shalini </a></p>
+        </div>
+</footer>
